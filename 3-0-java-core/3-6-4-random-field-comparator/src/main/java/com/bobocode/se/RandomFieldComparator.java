a85d8b3dc98810869aa5b164cce242da887d10d2
Bohdan Belokur <belokur.bv@gmail.com>
2022-12-29 14:56:32 +0200

    Homework

--- 3-0-java-core/3-6-4-random-field-comparator/src/main/java/com/bobocode/se/RandomFieldComparator.java
+++ 3-0-java-core/3-6-4-random-field-comparator/src/main/java/com/bobocode/se/RandomFieldComparator.java
@@ -2,13 +2,11 @@ package com.bobocode.se;
 
 import com.bobocode.data.Accounts;
 import com.bobocode.model.Account;
-import com.bobocode.util.ExerciseNotCompletedException;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
-import java.util.function.BiFunction;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
 
 /**
  * A generic comparator that is comparing a random field of the given class. The field is either primitive or
@@ -17,39 +15,22 @@ import java.util.stream.Collectors;
  * If no field is available to compare, the constructor throws {@link IllegalArgumentException}
  *
  * @param <T> the type of the objects that may be compared by this comparator
- *<p><p>
- *  <strong>TODO: to get the most out of your learning, <a href="https://www.bobocode.com/learn">visit our website</a></strong>
- *  <p>
- *
+ *            <p><p>
+ *            <strong>TODO: to get the most out of your learning, <a href="https://www.bobocode.com/learn">visit our website</a></strong>
+ *            <p>
  * @author Stanislav Zabramnyi
  */
 public class RandomFieldComparator<T> implements Comparator<T> {
-    public static final Map<String, Class<?>> WRAPPERS_MAP = Map.of(
-            "int", Integer.class,
-            "long", Long.class,
-            "float", Float.class,
-            "char",  Character.class,
-            "byte", Byte.class,
-            "short", Short.class,
-            "double", Double.class
-    );
-
     private Field compared;
+
+    private Class<T> clazz;
 
     public RandomFieldComparator(Class<T> targetType) {
         Objects.requireNonNull(targetType);
-        var fields = Arrays.stream(targetType.getDeclaredFields())
-                .peek(System.out::println)
+        this.compared = Arrays.stream(targetType.getDeclaredFields())
                 .filter(field -> Comparable.class.isAssignableFrom(field.getType()))
-                .toList();
-        if (fields.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        this.compared = fields.get(0);
-    }
-
-    String getRandomField(T object) {
-        return object.getClass().getDeclaredFields()[0].getName();
+                .findAny().orElseThrow(IllegalArgumentException::new);
+        this.clazz = targetType;
     }
 
     /**
@@ -59,48 +40,21 @@ public class RandomFieldComparator<T> implements Comparator<T> {
      * @param o1
      * @param o2
      * @return positive int in case of first parameter {@param o1} is greater than second one {@param o2},
-     *         zero if objects are equals,
-     *         negative int in case of first parameter {@param o1} is less than second one {@param o2}.
+     * zero if objects are equals,
+     * negative int in case of first parameter {@param o1} is less than second one {@param o2}.
      */
     @Override
     public int compare(T o1, T o2) {
         Objects.requireNonNull(o1);
         Objects.requireNonNull(o2);
-        var field = getComparingFieldName();
         try {
-            var field1= o1.getClass().getDeclaredField(field);
-            var field2= o2.getClass().getDeclaredField(field);
-
-            field1.setAccessible(true);
-            field2.setAccessible(true);
-
-            var f1 =  field1.get(o1);
-            var f2 =  field2.get(o2);
-            if (f1 == null && f2 == null) {
-                return 0;
-            } else if (f1 != null && f2 == null) {
-                return 1;
-            } else if (f2 != null && f1 == null) {
-                return -1;
-            }
-
-            if (field1.getType().isPrimitive() && field2.getType().isPrimitive()) {
-                var castClass = WRAPPERS_MAP.get(f1.getClass().getName());
-                f1 = f1.getClass().cast(castClass);
-                f2 = f2.getClass().cast(castClass);
-            }
-
-            return (int) f1.getClass().getDeclaredMethod("compareTo",
-                            o1.getClass().getDeclaredField(field).getType())
-                    .invoke(f1, f2);
-        } catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            compared.setAccessible(true);
+            Comparable f1 = (Comparable) compared.get(o1);
+            Comparable f2 = (Comparable) compared.get(o2);
+            return Comparator.<Comparable>nullsLast(Comparator.naturalOrder()).compare(f1, f2);
+        } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-        //throw new ExerciseNotCompletedException(); // todo: implement this method;
-    }
-
-    public static <U extends Comparable<? super U>> int compare(U o1, U o2) {
-        return 0;
     }
 
     /**
@@ -108,7 +62,6 @@ public class RandomFieldComparator<T> implements Comparator<T> {
      */
     public String getComparingFieldName() {
         return compared.getName();
-       // return "balance";
     }
 
     /**
@@ -119,15 +72,7 @@ public class RandomFieldComparator<T> implements Comparator<T> {
      */
     @Override
     public String toString() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method;
-    }
-
-    public static void main(String[] args) {
-        RandomFieldComparator<Account> randomFieldComparator = new RandomFieldComparator<>(Account.class);
-        var acc1 = Accounts.generateAccount();
-        var acc2 = Accounts.generateAccount();
-        System.out.println(acc1);
-        System.out.println(acc2);
-        System.out.println(randomFieldComparator.compare(acc1, acc2));
+        return String.format("Random field comparator of class '%s' is comparing '%s'", this.clazz.getSimpleName(),
+                compared.getName());
     }
 }
